6997f555e2ef81936608c411ecc5327544a4c11c
unknown <gvepkhvadze@gmail.com>
2020-06-29 16:01:09 +0400

    Upload Order

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,10 +9,8 @@
             
             <div class="col-lg-12">
                 <div class="about-content mt-30 text-center" style="padding-top:80px;">
-                    <h3> წყლის წარმომავლობა</h3>
-<p>საქართველო ერთ-ერთი მდიდარი ქვეყანაა თავისი ჰიდროგეოლოგიური რესურსებით, როგორც რაოდენობრივი, ასევე ხარისხობრივი მაჩვენებლების მიხედვით და წარმოადგენს დროში განახლებად რესურს. ჰიდროგეოლოგიურ რესურსებში მოიაზრება მტკნარი მინერალური და თერმული წყლები. 
+                    <h3> კონტაქტი</h3>
 
-</p>
                    
                 </div>
             </div>
@@ -38,28 +36,25 @@ background: linear-gradient(180deg, rgba(220,236,247,1) 0%, rgba(255,255,255,1) 
         <div class="row">
             <div class="col-lg-12">
                 <div class="contact-form pt-45">
-                    <h6>Leave Reply</h6>
                     <form id="contact-form" action="" data-toggle="validator">
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="singel-form form-group">
-                                    <label>Nick name :</label>
-                                    <input name="name" type="text" data-error="Name is required." required="required">
-                                    <div class="help-block with-errors"></div>
+                                    <input name="name" type="text" placeholder="სახელი" required="required">
+                                    
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="singel-form form-group">
-                                    <label>Email Address :</label>
-                                    <input type="email" name="email" data-error="Valid email is required." required="required">
-                                    <div class="help-block with-errors"></div>
+                                    <input type="email" name="email" placeholder="ელ ფოსტა" required="required">
+                                    
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="singel-form form-group">
-                                    <label>Write a message :</label>
-                                    <textarea name="message" data-error="Please,leave us a message." required="required"></textarea>
-                                    <div class="help-block with-errors"></div>
+                                    <label>შეტყობინება :</label>
+                                    <textarea name="message" placeholder="| მიუთითეთ შეტყობინება" required="required"></textarea>
+                                   
                                 </div>
                             </div>
                             <p class="form-message"></p>
@@ -79,7 +74,54 @@ background: linear-gradient(180deg, rgba(220,236,247,1) 0%, rgba(255,255,255,1) 
 
 
 
+<section id="contact-part" class="pt-30">
+    <div class="container">
 
+        <div class="col-lg-12">
+
+        <div class="row" style="background-color:#edf4f7; padding-top:30px;">
+
+            <div class="col-md-9">
+                <p style="font-size:25px;">შპს აქუა გეო</p>
+            </div>
+
+            <div class="col-md-3" align="right">
+                info@aquageo.ge
+            </div>
+
+        </div>
+
+
+        <div class="row" style="background-color:#edf4f7; padding-bottom:30px;">
+
+            <div class="col-md-5">
+                    <p style="font-size:14px;">საქართველო, მცხეთის რაიონი, სოფ. მისაქციელი 3308</p>
+            </div>
+
+            <div class="col-md-2">
+                +995 592 003 937
+            </div>
+
+            <div class="col-md-2">
+                +995 322 470 410
+            </div>
+
+            <div class="col-md-3" align="right">
+                Fax: +995 322 470 411
+            </div>
+
+
+        
+           
+        </div>
+
+    </div>
+
+
+
+
+    </div>
+</section>
 
 
 
